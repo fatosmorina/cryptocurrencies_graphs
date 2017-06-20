6d1052bfde783369d5c4542d29edc29f333d4115
Fatos Morina <fatosi.morina@gmail.com>
2017-06-20 14:41:23 +0200

    Allow null Currency values when they are missing from API

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,6 +1,5 @@
 class Currency < ApplicationRecord
 	validates :date, presence: true
-	validates :value, presence: true
 	validates :currency_type, presence: true
 
 	enum currency_type: [:bitcoin, :ethereum, :nasdaq]
