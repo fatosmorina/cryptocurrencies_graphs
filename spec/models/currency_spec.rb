e4705e69ae54a6083fb9969895d65feadfb3b14c
Fatos Morina <fatosi.morina@gmail.com>
2017-06-20 14:41:52 +0200

    Allow null Currency values when they are missing from API

--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Currency, type: :model do
 	it { should validate_presence_of(:date) }
-	it { should validate_presence_of(:value) }
 	it { should validate_presence_of(:currency_type) }
 
 	it 'should throw error for invalid value of currency_type' do
