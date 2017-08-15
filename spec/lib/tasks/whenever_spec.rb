c763fb1fdbfac201e855e5dc5bb956d58affe0f3
Fatos Morina <fatosi.morina@gmail.com>
2017-08-15 07:39:15 +0200

    Indent code at whenever_spec

--- spec/lib/tasks/whenever_spec.rb
+++ spec/lib/tasks/whenever_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 require 'whenever'
 
 describe 'task' do
-  	it 'makes sure `runner` statements exist' do
-		whenever = Whenever::JobList.new(file: Rails.root.join("config", "schedule.rb").to_s)
-   		 expect(whenever).to schedule_rake("currency:seed")
+  it 'makes sure `runner` statements exist' do
+    whenever = Whenever::JobList.new(file: Rails.root.join("config", "schedule.rb").to_s)
+    expect(whenever).to schedule_rake("currency:seed")
       .every(:day)
       .at("1am")
   end
