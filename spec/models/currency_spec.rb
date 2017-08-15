9ee85cef61081ff8b87d767a08a2b063477b0041
Fatos Morina <fatosi.morina@gmail.com>
2017-08-15 07:39:34 +0200

    Indent code at currency_spec

--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Currency, type: :model do
-	it { should validate_presence_of(:date) }
-	it { should validate_presence_of(:currency_type) }
+  it { should validate_presence_of(:date) }
+  it { should validate_presence_of(:currency_type) }
 
-	it 'should throw error for invalid value of currency_type' do
-		expect { build(:currency, currency_type: "invalid") }.to raise_error(ArgumentError).with_message(/is not a valid currency_type/)
-	end
+  it 'should throw error for invalid value of currency_type' do
+    expect { build(:currency, currency_type: "invalid") }.to raise_error(ArgumentError).with_message(/is not a valid currency_type/)
+  end
 end
 
 
